fe777b25cfe83f54d1152218496420b450e5886f
Rémi Attab <remi.attab@gmail.com>
2019-12-09 08:54:50 -0500

    Support for quoted atoms in token_to_list

--- test.erl
+++ test.erl
@@ -1,20 +1,22 @@
 
+% comment
+%% double comment
 main([Input, _ | _ ]) when
 Input == []
 ->
-    case
-        a + b
-    of
-        1 + 2 ->
-            blah;
-        _ ->
-            ok
-    end,
+  case
+     a_b@d + 'Bob'
+  of
+      1 + 2 ->
+          $a;
+      _ ->
+           ok
+  end,
+    %8mid comment
 
-
-    {ok, Data} = file:read_file(Input),
-    process(Data),
-    halt(0);
-main(_) ->
-    io:fwrite(standard_error, "usage: nedent <input> <output>~n", []),
-    halt(1).
+ {ok, Data} = file:read_file(Input),
+  process(Data),
+  halt(0);
+    main(_) ->
+   io:fwrite(standard_error, "usage: nedent <input> <output>~n", []),
+  halt(1).
